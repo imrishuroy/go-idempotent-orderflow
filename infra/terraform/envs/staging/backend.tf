b7e7b75f66b56004300ea2993b59de69893eac96
Rishu Kumar <rishukumar.prince@gmail.com>
2026-01-10 21:16:37 +0530

    fix the DynamoDB table that your IAM policy references

--- infra/terraform/envs/staging/backend.tf
+++ infra/terraform/envs/staging/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket        = "orderflow-tf-state"
-    key           = "staging/terraform.tfstate"
-    region        = "ap-south-1"
-    use_lockfile  = true
+    bucket         = "orderflow-tf-state"
+    key            = "staging/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "orderflow-tf-lock"
+    encrypt        = true
   }
 }
